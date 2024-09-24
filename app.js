98062ac50b1528d1d30adc124bfb947696cf48a2
connorsbastia <connorsbastia@gmail.com>
2024-09-24 18:42:05 +0400

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ document.getElementById('nb-trous').querySelector('span').textContent = nbTrous;
 document.getElementById('volume-injecte').querySelector('span').textContent = volumeInjecte.toFixed(1);
 
 // Envoi de l'e-mail via EmailJS
-emailjs.init("stephane"); 
+emailjs.init("connorsbastia@gmail.com"); 
 
 const templateParams = {
 nom: nom,
