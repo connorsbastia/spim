aa5d94353aec06c4d968725279fb19f9f6a42832
connorsbastia <connorsbastia@gmail.com>
2024-10-04 22:38:28 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,21 @@
-const CACHE_NAME = 'devis-app-cache-v1';
-const urlsToCache = [
-'./',
-'./index.html',
-'./app.js',
-'./manifest.json',
-'./icon-192x192.png',
-'./icon-512x512.png'
-];
-
-// Installer le Service Worker et mettre en cache les ressources
-self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => {
-console.log('Mise en cache des ressources');
-return cache.addAll(urlsToCache);
-})
-);
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/app.js',
+                '/icon.png'  // Assurez-vous d'avoir une icône avec ce nom ou ajustez le nom selon votre fichier
+            ]);
+        })
+    );
 });
 
-// Activer le Service Worker
-self.addEventListener('activate', event => {
-console.log('Service Worker activé');
-});
-
-// Intercepter les requêtes réseau et servir les fichiers en cache si disponibles
-self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => {
-return response || fetch(event.request);
-})
-);
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
